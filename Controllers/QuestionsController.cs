858d17d9ee0befa9f9df0c25040998fcedc15617
ruhollahm8 <graphist77@gmail.com>
2018-06-05 06:17:50 +0430

    Post & Get method | QuestionsController | QuizContext

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_bed.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        readonly QuizContext context;
+        public QuestionsController(QuizContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return context.questions;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Models.Question question)
+        {
+            context.questions.Add(question);
+            context.SaveChanges();
+        }
+    }
+}
