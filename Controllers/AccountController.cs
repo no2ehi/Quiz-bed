a5e44b84916b8f5905b0eac6a15d992680e95206
ruhollahm8 <graphist77@gmail.com>
2018-06-06 11:59:32 +0430

    Port Bug Fixed | Create Token | Login(interceptor)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,45 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
 
-namespace quiz_bed.Controllers
+namespace quiz_backend.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
+    public class Credentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
     {
         readonly UserManager<IdentityUser> userManager;
         readonly SignInManager<IdentityUser> signInManager;
+
         public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
-        public class Credentails
-        {
-            public string email { get; set; }
-            public string password { get; set; }
-        }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody]Credentails credentails)
+        public async Task<IActionResult> Register([FromBody] Credentials credentials)
         {
-            var user = new IdentityUser { UserName = credentails.email, Email = credentails.email };
+            var user = new IdentityUser { UserName = credentials.Email, Email = credentials.Email };
 
-            var result = await userManager.CreateAsync(user, credentails.password);
+            var result = await userManager.CreateAsync(user, credentials.Password);
 
             if (!result.Succeeded)
                 return BadRequest(result.Errors);
 
             await signInManager.SignInAsync(user, isPersistent: false);
 
-            var jwt = new JwtSecurityToken();
-            return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+            return Ok(CreateToken(user));
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Credentials credentials)
+        {
+            var result = await signInManager.PasswordSignInAsync(credentials.Email, credentials.Password, false, false);
+
+            if (!result.Succeeded)
+                return BadRequest();
+
+            var user = await userManager.FindByEmailAsync(credentials.Email);
+
+            return Ok(CreateToken(user));
+        }
+
+        string CreateToken(IdentityUser user)
+        {
+            var claims = new Claim[]
+           {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id)
+           };
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is the secret phrase"));
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+            var jwt = new JwtSecurityToken(signingCredentials: signingCredentials, claims: claims);
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
         }
     }
 }
--- Controllers/QuizzesController.cs
+++ Controllers/QuizzesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -84,6 +85,7 @@ namespace quiz_bed.Controllers
         }
 
         // POST: api/Quizzes
+ 
         [HttpPost]
         public async Task<IActionResult> PostQuiz([FromBody] Quiz quiz)
         {
